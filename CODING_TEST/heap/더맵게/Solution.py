a2b7747a210a8eefbb3956b34c3fd0ebc6f36c69
macho-yoo <85030719+macho-yoo@users.noreply.github.com>
2022-02-25 00:35:19 -0500

    Update Solution.py

--- CODING_TEST/heap/더맵게/Solution.py
+++ CODING_TEST/heap/더맵게/Solution.py
@@ -1,1 +1,18 @@
+import heapq
 
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] <= K :
+        if len(scoville) == 1 :
+            return -1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        new_food = first + (second *2)
+        
+        heapq.heappush(scoville,new_food)      
+        answer +=1
+        
+        
+    return answer
